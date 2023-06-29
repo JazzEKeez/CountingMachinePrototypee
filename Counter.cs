e42316f48fb29fd7e738be1f284c51967f8d6fe4
JazzEKeez <126212738+JazzEKeez@users.noreply.github.com>
2023-06-29 11:26:37 -0700

    Add files via upload

---
+++ Counter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public Text CounterText;
+
+    public float count = 0;
+
+    private void Start()
+    {
+        count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SphereScript sphere = other.GetComponent<SphereScript>();
+        if(sphere != null)
+        {
+            sphere.transform.SetParent(transform);
+            count += sphere.value;
+            CounterText.text = "" + count;
+        }  
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        SphereScript sphere = other.GetComponent<SphereScript>();
+        if(sphere != null)
+        {
+            sphere.transform.SetParent(null);
+            count -= sphere.value;
+            CounterText.text = "" + count;
+        }
+        
+    }
+}
